84af99a1832920122880a45cafbcdeeca4175c08
Gianni Bonelli <gianni.bonelli05@gmail.com>
2026-03-19 11:54:51 +0100

    Ridistribuzione delle funzioni sui diversi file

---
+++ mathf.h
@@ -0,0 +1,8 @@
+/** \file mathf.h
+ *  \brief Libreria per file main
+ *  \author Gianni Bonelli
+ */
+
+float Polynomial(float* coeff, int size, float in);
+void Rectangular(float* values, int size, float stepsize, float* integ1, float* integ2);
+float Trapezoidal(float* values, int size, float stepsize);
