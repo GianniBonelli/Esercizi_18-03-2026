6a28d0269241f36034b6b7d1b02f787ccb505da3
Gianni Bonelli <gianni.bonelli05@gmail.com>
2026-03-19 11:57:28 +0100

    Risoluzione problemi

--- mathf.h
+++ mathf.h
@@ -2,7 +2,12 @@
  *  \brief Libreria per file main
  *  \author Gianni Bonelli
  */
+#ifndef MATHF
+    #define MATHF
+        float Polynomial(float* coeff, int size, float in);
 
-float Polynomial(float* coeff, int size, float in);
-void Rectangular(float* values, int size, float stepsize, float* integ1, float* integ2);
-float Trapezoidal(float* values, int size, float stepsize);
+        void Rectangular(float* values, int size, float stepsize, float* integ1, float* integ2);
+
+        float Trapezoidal(float* values, int size, float stepsize);
+        
+#endif
